124a29c47e34b64e4dc4861e2816b9c0e011f415
fanfan <762349436@qq.com>
2020-08-30 12:30:39 +0800

    test

---
+++ src/test/java/test.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName test
+ * @Date
+ * @Author
+ * @Description TODO
+ **/
+public class test {
+
+    private String value;
+
+    public void test(){
+        System.out.println("test");
+    }
+
+}
