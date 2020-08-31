97dbfd488ad241f14f9581587b8a1c8775366df4
fanfan <762349436@qq.com>
2020-08-31 23:54:40 +0800

    beanFactory

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -12,8 +12,8 @@ public class test {
 
     @Test
     public void test(){
-        BeanFactory.add(Bean.class.getName(), new Bean(new Example("ff")));
-        Example example = (Example) BeanFactory.get(Bean.class.getName()).getObj();
+        BeanFactory.createBean(Bean.class.getName(), new Bean(new Example("ff")));
+        Example example = (Example) BeanFactory.getBean(Bean.class.getName()).getObj();
         example.example();
     }
 
